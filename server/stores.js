0ae8248192c9c2752b26b4170494f4ec8d10802d
Mikhail Malyshev <mlshv42@ya.ru>
2018-05-13 22:49:59 +0300

    Фикс /stores limit + offset

--- server/stores.js
+++ server/stores.js
@@ -12,8 +12,9 @@ const response = Object.values(storesData).map(
 );
 
 function stores(req, res) {
-    const offset = req.query.offset || 0;
-    const limit = req.query.limit || 10;
+    const offset = +req.query.offset || 0;
+    const limit = +req.query.limit || 10;
+
     res.send({
         payload: {
             stores: response.slice(offset, offset + limit),
